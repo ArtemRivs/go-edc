a4f6edcceb0e6ad56e90a7465b8b025086cace06
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-10-25 19:02:36 +0500

    Update structures03.go

---
+++ src/hello/structures03.go
@@ -0,0 +1,27 @@
+package solution
+
+import (
+	"strings"
+)
+
+// UserCreateRequest is a request to create a new user.
+type UserCreateRequest struct {
+	FirstName string
+	Age       int
+}
+func main(){
+
+}
+
+func Validate(req UserCreateRequest) string {
+
+	if req.FirstName == "" || strings.Contains(req.FirstName, " ") {
+		return "invalid request"
+	}
+	if req.Age <= 0 || req.Age > 150 {
+		return "invalid request"
+	}
+
+	return ""
+
+}
