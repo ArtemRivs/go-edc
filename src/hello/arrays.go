93c69691818b46d66df3a9ecfd2d765c4a7c219d
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-10-26 20:43:50 +0500

    Update arrays.go

--- src/hello/arrays.go
+++ src/hello/arrays.go
@@ -1,1 +1,13 @@
+package solution
 
+func SafeWrite(nums [5]int, i, val int) [5]int {
+
+	var arrМахIndex = len(nums) - 1;
+
+	if i >= 0 && i <= arrМахIndex {
+		nums[i] = val
+	}
+
+	return nums
+
+}
