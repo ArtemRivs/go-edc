86b2b2eac6c423ec5bd6203b62821e53f335d68f
Artem Rivs <artem.rivs@gmail.com>
2022-10-29 15:14:34 +0100

    Experiment with slice

--- src/hello/slices02.go
+++ src/hello/slices02.go
@@ -3,27 +3,43 @@ package main
 import "fmt"
 
 func main() {
-  nums[]int := {1, 2, 3, 4, 5, 6, 7, 8}
-  fmt.Println("Origin nums slice: ", nums)
-  Remove(nums, 5)
-  fmt.Println("Modified nums slice: ", nums)
-  
+	// nums := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	// fmt.Println("Origin nums slice: ", nums)
+	// Remove(nums, 5)
+	// fmt.Println("Modified nums slice: ", nums)
+
+	nums := []string{"one", "two", "three", "four", "five", "six", "seven", "eight"}
+	Remove(nums, 1)
+	fmt.Println("Modified nums slice: ", nums)
+
 }
-func Remove(nums []int, i int) []int {
+func Remove(nums []string, i int) []string {
 
-	if i < 0 || i >= len(nums) {
-		return nums
-	}
+	// if i < 0 || i >= len(nums) {
+	// 	return nums
+	// }
 
-	var newNums[]int
-	for (j := 0; j < len(nums); j++) {
-		if j == i {
-			continue
-		}
-		append(newNums, nums[j])
-	}
+	// var newNums []int
+	// for j := 0; j < len(nums); j++ {
+	// 	if j == i {
+	// 		continue
+	// 	}
+	// 	newNums = append(newNums, nums[j])
+	// }
 
-	nums = newNums
+	// nums = newNums
+
+	// fmt.Println("newNums slice: ", newNums)
+
+	// return nums
+
+	//nums[i] = nums[len(nums)-1]
+
+	//fmt.Println("nums slice: ", nums[:len(nums)-1])
+
+	// nums = append(nums[:i], nums[i+1:]...)
+	nums = append(nums[:i], nums[i+1:]...)
+	fmt.Println("nums sliced: ", nums)
 
 	return nums
 
