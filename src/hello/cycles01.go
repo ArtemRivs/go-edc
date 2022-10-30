3a94b2d59c5fb8bad2b9a85cd9578aa4e511b595
Artem Rivs <artem.rivs@gmail.com>
2022-10-30 14:30:06 +0000

    Change cycles01.go

--- src/hello/cycles01.go
+++ src/hello/cycles01.go
@@ -8,11 +8,16 @@ func main() {
 
 	array := make([]int, 0, 10)
 
-	for i := 0; i < 10; i++ {
-		array = append(array, i*5)
-	}
-	// array := make([]int, 32, 32)
+	// for i := 0; i < 10; i++ {
+	// 	array = append(array, i*5)
+	// }
 
+	i := 0
+
+	for i < 10 {
+		array = append(array, i)
+		i++
+	}
 	fmt.Println("array: ", array)
 	fmt.Println("array len: ", len(array))
 
