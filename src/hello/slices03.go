6f58b2c1a4380585493d2b9fbd36c0697996cb00
Artem Rivs <artem.rivs@gmail.com>
2022-10-30 12:01:25 +0000

    Experiment width slices03.go

--- src/hello/slices03.go
+++ src/hello/slices03.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 func main() {
 
-	array := []int{1, 2, 3, 4, 5, 6}
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+	// array := make([]int, 32, 32)
+
 	fmt.Println("array: ", array)
-	fmt.Println("array size: ", unsafe.Sizeof(array))
+	fmt.Println("array len: ", len(array))
+
+	slice01 := array[:3]
+	fmt.Println("slice01 [:3] len: ", len(slice01))
+	fmt.Println("slice01 [:3]: ", slice01)
+
+	slice02 := array[3:7]
+	fmt.Println("slice02 [3:7] len: ", len(slice02))
+	fmt.Println("slice02 [3:7]: ", slice02)
+
+	slice03 := array[5:]
+	fmt.Println("slice03 [5:] len: ", len(slice03))
+	fmt.Println("slice03 [5:]: ", slice03)
+
+	slice04 := array[1:11]
+	fmt.Println("slice04 [1:11] len: ", len(slice04))
+	fmt.Println("slice04 [1:11]: ", slice04)
 
 }
