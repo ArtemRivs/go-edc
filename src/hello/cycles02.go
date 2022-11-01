f030e6f98e5c8ec0dd02a5fcbc1205e42ccd4f5c
Artem Rivs <artem.rivs@gmail.com>
2022-11-01 14:26:29 +0000

    Experiments with cycles02

--- src/hello/cycles02.go
+++ src/hello/cycles02.go
@@ -1,11 +1,30 @@
-package solution
+package main
 
-func Map(strs []string, mapFunc func(s string) string) []string {
+// func Map(strs []string, mapFunc func(s string) string) []string {
+// 	slice := make([]string, len(strs))
+// 	for i, str := range strs {
+//     	slice[i] = mapFunc(str)
+// 	}
+// 	return slice
+// }
 
-	slice := make([]string, len(strs)) 
-	for i, str := range strs {
-    	slice[i] = mapFunc(str)
-	}
-	return slice
+import "fmt"
+
+func main() {
+
+	// slice := []string{"one", "two", "three", "four", "five", "six"}
+	// for i := 0; i < len(slice); i++ {
+	// 	fmt.Println("index: ", i, "value: ", slice[i])
+
+	// }
+
+	slice := make([]string, 0, 6)
+	slice = append(slice, "one")
+	slice = append(slice, "two")
+	slice = append(slice, "three")
+	slice = append(slice, "four")
+	slice = append(slice, "five")
+	slice = append(slice, "six")
+	fmt.Println("slice: ", slice)
 
 }
