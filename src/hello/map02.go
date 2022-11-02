3ff9bf6528d288d87d5389abbba650c78c5474c3
Artem Rivs <artem.rivs@gmail.com>
2022-11-02 17:47:29 +0000

    Add  map02.go

---
+++ src/hello/map02.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+	IDs := []int64{67, 47, 27, 33, 834, 26, 10, 44, 81}
+	m := map[int64]bool{}
+
+	for pointer, id := range IDs {
+
+	}
+
+	// for c, value := range str {
+	// 	fmt.Println("c: ", c, "value: ", value)
+	// }
+
+}
