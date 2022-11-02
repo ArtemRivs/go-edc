b478c1cd732c61d39521bda6554c8c6a840fb03c
Artem Rivs <artem.rivs@gmail.com>
2022-11-02 13:56:17 +0000

    Experimet 01 with sorting02.go

--- src/hello/sorting02.go
+++ src/hello/sorting02.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 
-	nums := []int{7, 4, 5, 1, 2}
-	fmt.Println("nums", nums)
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
-	})
-	fmt.Println("nums", nums)
+	// nums := []int{7, 4, 5, 1, 2}
+	// fmt.Println("nums", nums)
+	// sort.Slice(nums, func(i, j int) bool {
+	// 	return nums[i] < nums[j]
+	// })
+	// fmt.Println("nums", nums)
 
-	nums2 := []int{8, 4, 1, 4, 5, 4, 9}
+	nums2 := []int64{8, 4, 1, 4, 5, 4, 9}
 	fmt.Println("nums2", nums2)
 	sort.SliceStable(nums2, func(i, j int) bool {
 		return nums2[i] < nums2[j]
