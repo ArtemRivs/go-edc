81f0f4caa5bb814b06e7b97bcd8d69f714ddd1f3
Artem Rivs <artem.rivs@gmail.com>
2022-11-02 17:25:50 +0000

    Change  map01.go

--- src/hello/map01.go
+++ src/hello/map01.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	m := map[int]string{1: "one", 2: "two", 3: "three"}
-	fmt.Println("m: ", m)
+	// m := map[int]string{1: "one", 2: "two", 3: "three"}
+	// fmt.Println("m: ", m)
+
+	str := map[string]int{"one": 1, "two": 2, "three": 3}
+	// fmt.Println("str: ", str)
+
+	for c, value := range str {
+		fmt.Println("c: ", c, "value: ", value)
+	}
+	// fmt.Println("str['one']: ", str["one"])
+	// fmt.Println("len(str): ", len(str))
+	fmt.Println("str['one']: ", str["_one"])
+
 }
