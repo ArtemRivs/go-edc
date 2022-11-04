0d199dd52d5068ae4465ca1ac4ee795babfc0eff
Artem Rivs <artem.rivs@gmail.com>
2022-11-04 11:49:42 +0000

    Final version of map03.go

--- src/hello/map03.go
+++ src/hello/map03.go
@@ -1,23 +1,30 @@
-package solution
+package main
 
-func MostPopularWord(words []string) string {
+import "fmt"
 
-	// wm := map[string]int{}
-	wm := make(map[string]int,0)
+func main() {
+
+	words := []string{"hello", "world", "hello", "world", "world"}
+	wm := make(map[string]int, 0)
 	for _, word := range words {
 		if val, ok := wm[word]; ok {
 			wm[word] = val + 1
+			continue
 		}
+		wm[word] = 1
 	}
+	fmt.Println("words: ", words)
+	fmt.Println("wm: ", wm)
 
 	var maxWord string
-	var maxValue int 
+	var maxValue int
 	for key, val := range wm {
 		if val > maxValue {
 			maxValue = val
 			maxWord = key
 		}
 	}
-	
-	return maxWord
+
+	fmt.Println("maxWord: ", maxWord)
+
 }
