d09004293547f355ba3654c2c9998cd71ef868f5
Artem Rivs <artem.rivs@gmail.com>
2022-11-05 10:35:12 +0000

    Update rune.go

--- src/hello/rune.go
+++ src/hello/rune.go
@@ -1,14 +1,27 @@
-package solution
+package main
 
 // import "unicode"
+import "fmt"
 
-func  isASCII(s string) bool{
+func main() {
 
-	res := false
+	s := "Hello!"
+	fmt.Println("isASCII", isASCII(s))
+
+}
+
+func isASCII(s string) bool {
+
+	res := true
 	strSlice := []rune(s)
+
+	fmt.Println("strSlice:", strSlice)
+
 	for _, ch := range strSlice {
+		fmt.Println("ch:", ch, string(ch))
+
 		if ch < 0 || ch > 255 {
-			res = true
+			res = false
 			break
 		}
 	}
