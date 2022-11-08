dc5816fbe1db6aa4c4783555ea7e3951b64e1b04
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-11-08 19:30:47 +0500

    Update types02.go

--- src/hello/types02.go
+++ src/hello/types02.go
@@ -3,7 +3,13 @@ import "fmt"
  
 type mile int
 type kilometer int
+
+type person struct{
+    name string
+    age int
+}
  
+
 func distanceToEnemy (distance mile){
      
     fmt.Println("расстояние для противника:")
@@ -16,4 +22,21 @@ func main() {
     distanceToEnemy(distance)
     // var distance2 kilometer = 5
     // distanceToEnemy(distance2)   // ! ошибка
+ 
+ 
+     // var tom person
+    // tom.name = "Tom"
+    // tom.age = 23
+
+    // var tom person = person{"Tom", 23}
+    
+    tom := person{"Tom", 23}
+    alice := person{"Alice", 30}
+    
+
+    fmt.Println("tom:", tom)
+    fmt.Println("alice:", alice)
+ 
+ 
+ 
 }
