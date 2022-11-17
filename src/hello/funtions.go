27befdf911666be0967d72ff84f5b403ccef79ca
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-11-17 17:59:09 +0500

    Update funtions.go

--- src/hello/funtions.go
+++ src/hello/funtions.go
@@ -5,3 +5,21 @@ import "strconv"
 func IntToString(value int) string {
 	return strconv.Itoa(value)
 }
+
+
+func Generate(seed int) func() {
+    return func() {
+        fmt.Println(seed) // замыкание получает внешнюю переменную seed
+        seed += 2 // переменная модифицируется
+    }
+    
+}
+
+func main() {
+    iterator := Generate(0)
+    iterator()
+    iterator()
+    iterator()
+    iterator()
+    iterator()
+}
