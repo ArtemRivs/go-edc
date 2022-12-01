a25e5f490cb0d18f684d126e748417076a32a0b2
Artem Rivs <artem.rivs@gmail.com>
2022-12-01 19:22:45 +0000

    Changes interface02.go

--- src/interface02.go
+++ src/interface02.go
@@ -5,13 +5,20 @@ import "fmt"
 type Square struct {
 	length float32
 }
-
+type Rectangle struct {
+	firstSide  float32
+	secondSide float32
+}
 type Circle struct {
 	radius float32
 }
 
 type Shape interface {
 	Area() float32
+}
+
+func (r Rectangle) Area() float32 {
+	return r.firstSide * r.secondSide
 }
 
 func (l Square) Area() float32 {
@@ -31,6 +38,9 @@ func main() {
 	fmt.Println("GO!")
 	a := Square{length: 15.5}
 	b := Circle{radius: 10.4}
+	c := Rectangle{firstSide: 1.8, secondSide: 4.2}
 	printShapeArea(a)
 	printShapeArea(b)
+	printShapeArea(c)
+
 }
