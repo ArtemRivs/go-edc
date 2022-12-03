7777a6112d472fe9aac290012853c3c5c73b5680
Artem Rivs <artem.rivs@gmail.com>
2022-12-03 13:48:21 +0000

    Change interface03.go

--- src/interface03.go
+++ src/interface03.go
@@ -1,1 +1,33 @@
 package main
+
+import "fmt"
+
+type Shape interface {
+	getArea() float32
+}
+type Circle struct {
+	radius float32
+}
+type Square struct {
+	length float32
+}
+
+func (c Circle) getArea() float32 {
+	return c.radius * c.radius * 3.14
+}
+
+func (s Square) getArea() float32 {
+	return s.length * s.length
+}
+
+func main() {
+
+	var S1 Shape = Circle{radius: 200.5}
+	var S2 Shape = Square{length: 9999999.5}
+
+	// fmt.Println(S1)
+	// fmt.Println(S2)
+	fmt.Println("Circle area: ", S1.getArea())
+	fmt.Println("Square area: ", S2.getArea())
+
+}
