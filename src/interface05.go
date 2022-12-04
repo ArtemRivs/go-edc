0a26c9e946c66ada9c487ccd7b0d4605421d33b1
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-12-04 20:50:27 +0500

    Create interface05.go

---
+++ src/interface05.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+)
+
+type TapePlayer struct {
+  paramTP string
+}
+type CDPlayer struct {
+  paramCD string
+}
+
+func (t TapePlayer) Play(song string) {
+  fmt.Println("Playing song (TP): ", song)
+}
+func (t TapePlayer) Stop() {
+  fmt.Println("Stop playing")
+}
+func (t TapePlayer) Record() {
+  fmt.Println("Recording...")
+}
+func (t CDPlayer) Play(song string) {
+  fmt.Println("Playing song (CD): ", song)
+}
+func (t CDPlayer) Stop() {
+  fmt.Println("Stop playing")
+}
+
+func runPlayList(device TapePlayer, songs[] string){
+  for _, song = range songs {
+    device.Play(song)
+  }
+  device.Stop()
+  
+}
