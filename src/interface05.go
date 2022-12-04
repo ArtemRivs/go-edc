b4a67538c11c6d4a5a9fa9e07785c5abe90f3a86
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-12-04 21:04:51 +0500

    Update interface05.go

--- src/interface05.go
+++ src/interface05.go
@@ -1,36 +1,41 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type TapePlayer struct {
-  paramTP string
+	paramTP string
 }
 type CDPlayer struct {
-  paramCD string
+	paramCD string
 }
 
 func (t TapePlayer) Play(song string) {
-  fmt.Println("Playing song (TP): ", song)
+	fmt.Println("Playing song (TP): ", song)
 }
 func (t TapePlayer) Stop() {
-  fmt.Println("Stop playing")
+	fmt.Println("Stop playing")
 }
 func (t TapePlayer) Record() {
-  fmt.Println("Recording...")
+	fmt.Println("Recording...")
 }
 func (t CDPlayer) Play(song string) {
-  fmt.Println("Playing song (CD): ", song)
+	fmt.Println("Playing song (CD): ", song)
 }
 func (t CDPlayer) Stop() {
-  fmt.Println("Stop playing")
+	fmt.Println("Stop playing")
 }
 
-func runPlayList(device TapePlayer, songs[] string){
-  for _, song = range songs {
-    device.Play(song)
-  }
-  device.Stop()
-  
+func runPlayList(device TapePlayer, songs []string) {
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
+
+func main() {
+	mix := []string{"Song 2", "Smile", "Nevermind"}
+	device := TapePlayer{}
+	runPlayList(device, mix)
 }
