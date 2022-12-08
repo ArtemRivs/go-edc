f15e9843e172c4dc23d47cf22ad03a0effc514ae
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-12-08 23:38:18 +0500

    Update interface06.go

--- src/interface06.go
+++ src/interface06.go
@@ -5,6 +5,27 @@ import (
 	"reflect"
 )
 
+type MyType struct{}
+
+func IsNil(obj interface{}) bool {
+    if obj == nil {
+        return true
+    }
+
+    objValue := reflect.ValueOf(obj)
+    // проверяем, что тип значения ссылочный, то есть в принципе может быть равен nil
+    if objValue.Kind() != reflect.Ptr {
+        return false
+    } 
+    // проверяем, что значение равно nil 
+    //  важно, что IsNil() вызывает панику, если value не является ссылочным типом. Поэтому всегда проверяйте на Kind() 
+    if  objValue.IsNil() {
+        return true
+    }
+
+    return false
+} 
+
 func main() {
 
   var varBool *bool
