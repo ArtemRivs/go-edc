37c66dfb5155236b05f192b8add1560de7f0978b
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2022-12-11 22:00:41 +0500

    Update panic.go

--- src/panic.go
+++ src/panic.go
@@ -1,10 +1,24 @@
-import "fmt"
+package main
+
+import (
+	"errors"
+	"fmt"
+)
 
 func mypanic() {
-    var slice []string
-    fmt.Println(slice[0])
+	defer func() error {
+		if r := recover(); r != nil {
+			e := errors.New("Panic")
+			fmt.Println("Panic!: ", r)
+			return e
+		}
+		return nil
+	}()
+	var slice []string
+	fmt.Println(slice[0])
 }
 
 func main() {
-    mypanic()
-} 
+	mypanic()
+
+}
